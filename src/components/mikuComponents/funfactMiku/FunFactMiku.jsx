75ef3cad56920b6b48269c599f37e2b00e94b0b9
LeahJKH <leahjb06.kodehode@gmail.com>
2024-04-14 16:50:31 +0200

    get info from arr on github

--- src/components/mikuComponents/funfactMiku/FunFactMiku.jsx
+++ src/components/mikuComponents/funfactMiku/FunFactMiku.jsx
@@ -1,14 +1,27 @@
-import  Mikufacts from "./FunFactData.json"
-
+import { useState, useEffect } from "react";
+``
 export default function FunFactMiku() {
-    
-    let randomNumber = Math.floor(Math.random() * Mikufacts.length )
+    const [facts, setFacts] = useState([]);
+    const url = "https://leahjkh.github.io/mikuviteInfo/json/FunFactsMiku.json";
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(url);
+                const data = await response.json();
+                setFacts(data);
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        };
+        fetchData();
+    }, [url]);
+        const randomNumber = Math.floor(Math.random() * facts.length);
     return (
         <>
-        <div>
-            <img src="" alt="" />
-            <p>- {JSON.stringify(Mikufacts[randomNumber])}</p>
-        </div>
+            <div>
+                <img src="" alt="" />
+                <p>- {facts[randomNumber]}</p>
+            </div>
         </>
-    )
+    );
 }
