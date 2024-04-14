5be1fc87012f6b925a954da782912ce66176c42c
LeahJKH <leahjb06.kodehode@gmail.com>
2024-04-14 13:31:21 +0200

    first

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Footer from './components/layout/footer/Footer'
+import './App.css'
+import {MikuLove} from "@johnb08/test-library"
+
+function App() {
+
+  return (
+    <>
+      <MikuLove></MikuLove>
+      <Footer></Footer>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/layout/footer/Footer.jsx
@@ -0,0 +1,25 @@
+import Styles from "./Footer.css"
+export default function Footer() {
+    return (
+        <>
+        <footer className={Styles.FooterStyles}>
+            <div className={Styles.emailBar}>
+                <input type="text" name="email-field" id="emailField" placeholder="Enter Email" className={Styles.emailFieldStyling}/>
+                <p className={Styles.notSoTrueInfo}>the rights too this website is owned by me and miku as we are married</p>
+                <img src="" alt="" className={Styles.logoImg}/>
+            </div>
+
+            <div className={Styles.linkBar}>
+                <a href="#" className={Styles.barLinks}>privacy policy</a>
+                <a href="#" className={Styles.barLinks}>Terms and conditions</a>
+                <a href="#" className={Styles.barLinks}>Hunger tax</a>
+            </div>
+
+            <div className={Styles.infoBar}>
+                <p className={Styles.infoText}>copyright 2026</p>
+                <p className={Styles.InfoText}>this site is satirical</p>
+            </div>
+        </footer>
+        </>
+    )
+}
